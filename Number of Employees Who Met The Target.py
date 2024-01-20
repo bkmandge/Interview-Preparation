ae958a8d35992e66cee0f1f6d9a0a0350d3d63d9
bkmandge@gmail.com <bkmandge@gmail.com>
2024-01-20 20:09:04 +0530

    Count Number of Employees

--- Number of Employees Who Met The Target.py
+++ Number of Employees Who Met The Target.py
@@ -1,4 +1,21 @@
 """
                 Number of Employees Who Met The Target
 
+Input: hours = [0,1,2,3,4], target = 2
+Output: 3
+
 """
+
+
+def numberOfEmployeesWhoMetTarget(hours, target):
+    emp_count = 0
+    for i in range(len(hours)):
+        if hours[i] >= target:
+            emp_count += 1
+    return emp_count
+
+
+hours = [0,1,2,3,4]
+target = 2
+
+print(numberOfEmployeesWhoMetTarget(hours, target))

300424b0c4cb921288a7bac5a1ee163fd59a3985
bkmandge <bkmandge@gmail.com>
2024-03-01 08:57:56 +0530

    count frequency of each element

---
+++ count frequency of each element.py
@@ -0,0 +1,37 @@
+# Brute Force Approach:- TC O(N2), SC O(N)
+def countFrequency(arr):
+    n = len(arr)
+    visited = [False] * n
+    
+    for i in range(n):
+        if visited[i] == True:
+            continue
+        count = 1
+        
+        for j in range(i + 1, n):
+            if arr[i] == arr[j]:
+                visited[j] = True
+                count += 1
+        
+        print(arr[i], count)
+        
+
+# Hashmap:- TC O(N), O(N)
+def countFrequency2(arr):
+    n = len(arr)
+    hashMap = {}  
+        
+    for i in range(n):
+        if arr[i] in hashMap:
+            hashMap[arr[i]] += 1  # increase count
+        else:
+            hashMap[arr[i]] = 1 # add element with count 1
+    
+    for i in hashMap:
+        print(i, hashMap[i])  # print key - value / count pairs
+    
+    
+arr = [10,5,10,15,10,5]
+print(arr)
+countFrequency2(arr)
+        

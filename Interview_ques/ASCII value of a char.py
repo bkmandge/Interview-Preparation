20d96836601a54cfac2e440e6454759af99d85ba
bkmandge <bkmandge@gmail.com>
2024-03-04 10:58:15 +0530

    ASCII value of a char

---
+++ Interview_ques/ASCII value of a char.py
@@ -0,0 +1,20 @@
+"""                     ASCII value of a char
+
+In Python, the ord() function is used to get the ASCII value of a character.
+
+char = "A" -> 65
+char = "e" -> 101
+
+T.C. O(1)
+S.C. O(1)
+
+"""
+
+def ASCIIvalue(char):
+    print(f"ASCII value of {char} is: {ord(char)}")
+
+
+ASCIIvalue('A')    
+ASCIIvalue('e')
+
+

127c2f767ac559b6daecc92a580f78a1bda9af9a
bkmandge <bkmandge@gmail.com>
2024-03-05 11:43:25 +0530

    for loop

--- Find Words Containing Character.py
+++ Find Words Containing Character.py
@@ -8,14 +8,15 @@ Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indi
 Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
 Output: [0, 2]
 
-Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
 Output: []
 """
 
 from typing import *
 def findWordsContaining(words: List[str], x: str) -> List[int]:
+    n = len(words)
     idx = []
-    for i in range(len(words)):
+    for i in range(n):
         if x in words[i]:
             idx.append(i)
     return idx

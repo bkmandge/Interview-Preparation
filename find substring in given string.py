ad44a84b99b5cf38d2bda0feb85d587ea80a9035
bkmandge <bkmandge@gmail.com>
2024-03-08 08:51:39 +0530

    find substring in given string

--- find substring in given string.py
+++ find substring in given string.py
@@ -19,4 +19,31 @@ t = "cde"
 
 print(findSubstring(s, t))
 
-    
+
+def strStr(haystack, needle):
+    if not needle:
+        return 0
+
+    if not haystack:
+        return -1
+
+    n = len(needle)
+    h = len(haystack)
+
+    # iterates through the haystack string using a sliding window
+    #  window size is equal to the needle string size
+    # haystack[i : i + n] i -> starting index
+    # i + n -> ending index of the substring you want to extract
+    for i in range(h - n + 1):
+        if haystack[i: i + n] == needle:
+            return i
+    return -1
+
+
+haystack1 = "sadbutsad"
+needle1 = "sad"  # 0
+haystack2 = "leetcode"
+needle2 = "leeto"  # -1
+
+
+

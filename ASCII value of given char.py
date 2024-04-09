55d45f1d1babcea87724f8a8f06c108f8f50cf60
bkmandge <bkmandge@gmail.com>
2024-04-09 08:42:11 +0530

    use ord()

---
+++ ASCII value of given char.py
@@ -0,0 +1,11 @@
+"""
+                            American Standard Code for Information Interchange
+                            
+128 chars -> from 0 to 127
+-> Alphabets (65 – 90  &  97 – 122)
+-> Digits (48 – 57)
+-> Remaining are Special Character ( !, @, #, $, * …..)                            
+
+"""
+char = input("enter ay char: ")
+print(ord(char))
